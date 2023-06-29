f53ba7221c958609d2b0072d3c9d5483226494ce
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 17:10:57 -0300

    Update Ej11Java.java

--- Ej11Java.java
+++ Ej11Java.java
@@ -1,26 +1,17 @@
+
 import java.util.Scanner;
 
-public class MayorDeTres {
+public class VerificarVocal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese el valor de la primera variable: ");
-        int variable1 = scanner.nextInt();
+        System.out.print("Ingrese un carácter: ");
+        char caracter = scanner.next().charAt(0);
 
-        System.out.print("Ingrese el valor de la segunda variable: ");
-        int variable2 = scanner.nextInt();
-
-        System.out.print("Ingrese el valor de la tercera variable: ");
-        int variable3 = scanner.nextInt();
-
-        int mayor = variable1;
-
-        if (variable2 > mayor && variable2 > variable3) {
-            mayor = variable2;
-        } else if (variable3 > mayor && variable3 > variable2) {
-            mayor = variable3;
+        if (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u') {
+            System.out.println("El carácter ingresado es una vocal.");
+        } else {
+            System.out.println("El carácter ingresado no es una vocal.");
         }
-
-        System.out.println("La mayor de las tres variables es: " + mayor);
     }
 }
