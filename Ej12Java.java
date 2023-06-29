8b552195bfa16e046b0b9bd0f881d6c3cf95ae41
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 17:12:09 -0300

    Update Ej12Java.java

--- Ej12Java.java
+++ Ej12Java.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class DeterminarDocena {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (numero >= 1 && numero <= 12) {
+            System.out.println("El número está en la primera docena.");
+        } else if (numero >= 13 && numero <= 24) {
+            System.out.println("El número está en la segunda docena.");
+        } else if (numero >= 25 && numero <= 36) {
+            System.out.println("El número está en la tercera docena.");
+        } else {
+            System.out.println("El número no pertenece a ninguna docena válida.");
+        }
+    }
+}
