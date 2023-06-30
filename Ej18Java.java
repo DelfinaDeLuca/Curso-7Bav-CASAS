d566262c9a05aea788fbca5f96aa599cca07938a
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 21:10:38 -0300

    Update Ej18Java.java

--- Ej18Java.java
+++ Ej18Java.java
@@ -2,14 +2,13 @@ public class Ej18Java {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Tabla de multiplicar del " + i + ":");
-            System.out.println("-------------------------");
 
             for (int j = 1; j <= 10; j++) {
                 int resultado = i * j;
                 System.out.println(i + " x " + j + " = " + resultado);
             }
 
-            System.out.println();
+            System.out.println(); // Separador entre tablas
         }
     }
 }
