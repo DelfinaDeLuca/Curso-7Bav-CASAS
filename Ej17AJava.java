f18958c91df899247b3c510f99a18dc52f0e68a6
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 22:08:12 -0300

    Update Ej17AJava.java

--- Ej17AJava.java
+++ Ej17AJava.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvaluacionAlumno {
+public class Ej17AJava {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
