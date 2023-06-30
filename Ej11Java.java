256562a9d85e66290b036c9d4fb6abad840fe3ef
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 22:06:49 -0300

    Update Ej11Java.java

--- Ej11Java.java
+++ Ej11Java.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class VerificarVocal {
+public class Ej11Java {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
