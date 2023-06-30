5772c3f55123f3db441d2fb78652851b020b94d8
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 22:07:06 -0300

    Update Ej12Java.java

--- Ej12Java.java
+++ Ej12Java.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DeterminarDocena {
+public class Ej12Java {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
