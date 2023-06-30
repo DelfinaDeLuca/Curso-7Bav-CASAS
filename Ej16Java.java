9d5f6329d22f27f78b23ffcf1f6e3bd86b6907f2
DelfinaDeLuca <138087489+DelfinaDeLuca@users.noreply.github.com>
2023-06-29 21:05:35 -0300

    Update and rename EjDIECISEIS Java.java to Ej16Java.java

---
+++ Ej16Java.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un valor: ");
+        int valor = scanner.nextInt();
+
+        System.out.println("Tabla de multiplicar del " + valor + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = valor * i;
+            System.out.println(valor + " x " + i + " = " + resultado);
+        }
+    }
+}
